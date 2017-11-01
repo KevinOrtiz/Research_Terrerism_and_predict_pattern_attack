c1b0f3a7f25a56782cb20096b646ae4b60b9883c
KevinOrtiz <kevanort@espol.edu.ec>
2017-11-01 07:27:02 -0500

    Add code of execute

--- script_streamming_eventos_terrorism/streamming_twitter.py
+++ script_streamming_eventos_terrorism/streamming_twitter.py
@@ -47,3 +47,12 @@ class StreamListener(tweepy.StreamListener):
         except Exception as e:
             print(e)
 
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#Set up the listener. The 'wait_on_rate_limit=True' is needed to help with Twitter API rate limiting.
+listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
+streamer = tweepy.Stream(auth=auth, listener=listener)
+print("Tracking: " + str(WORDS))
+streamer.filter(track=WORDS)
